cc60a2024dca824ba9199d7cb07493dfe519e140
Simon Svendsgaard Nielsen <simon97nielsen@gmail.com>
2021-03-25 08:02:09 +0100

    Remove unused bottom from AppBar

--- client/lib/widgets/app_bar.dart
+++ client/lib/widgets/app_bar.dart
@@ -4,19 +4,16 @@ class MapTogetherAppBar extends StatelessWidget implements PreferredSizeWidget {
   MapTogetherAppBar(
       {Key key,
       @required this.title,
-      this.bottom,
       this.actionButtons})
       : super(key: key);
 
   String title;
-  PreferredSizeWidget bottom;
 
   List<IconButton> actionButtons;
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      bottom: bottom,
       title: Text(title),
       backgroundColor: Colors.lightGreen,
       actions: actionButtons,
