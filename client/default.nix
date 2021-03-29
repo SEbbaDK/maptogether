ec62d798c80f9ef2e75a726e14da22422870a927
SEbbaDK <sebbadk@gmail.com>
2021-03-29 15:07:01 +0200

    Add client Flutter submodule

--- client/default.nix
+++ client/default.nix
@@ -1,9 +1,9 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/45a014f4796a0b3051652ae23d7bcb72823cb742.tar.gz") { config.android_sdk.accept_license = true; } }:
 let
-  btv = "30.0.3";
+  btv = "29.0.2";
   android = pkgs.androidenv.composeAndroidPackages {
-    buildToolsVersions = [ btv ];
-    platformVersions = [ "30" ];
+    buildToolsVersions = [ "28.0.3" btv ];
+    platformVersions = [ "28" "29" ];
   };
   sdk = "${android.androidsdk}";
 in
