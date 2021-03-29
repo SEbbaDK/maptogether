e2f6cdaa8cb0bd030f1906eec05f289fe29230d0
Simon Svendsgaard Nielsen <simon97nielsen@gmail.com>
2021-03-29 17:10:34 +0200

    Added new buttons on map screen

--- client/lib/screens/map_screen.dart
+++ client/lib/screens/map_screen.dart
@@ -1,6 +1,4 @@
-import 'package:client/screens/page2.dart';
 import 'package:client/screens/social_screen.dart';
-import 'package:client/widgets/app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
@@ -11,7 +9,7 @@ class MapScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MapTogetherAppBar(
+      /* appBar: MapTogetherAppBar(
         title: 'MapTogether',
         actionButtons: [
           IconButton(
@@ -30,7 +28,99 @@ class MapScreen extends StatelessWidget {
           ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
+
+       */
+      body: Stack(
+        children: [
+          Container(
+            child: Center(
+              child: FlutterMap(
+                options: MapOptions(
+                  center: LatLng(57.04, 9.92),
+                  zoom: 12.0,
+                ),
+                layers: [
+                  TileLayerOptions(
+                    urlTemplate:
+                        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                    subdomains: ['a', 'b', 'c'],
+                    tileProvider: NetworkTileProvider(),
+                  ),
+                  //MarkerLayerOptions(markers: markers)
+                ],
+              ),
+            ),
+          ),
+
+
+          // Positioned button container dims ...
+          Row(
+            children: [
+
+            ],
+          ),
+
+          Positioned(
+            right: 5,
+            bottom: 5,
+            child: SafeArea(
+              // avoids rounded screen problem
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.lightGreen,
+                  //shape: BoxShape.circle,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: IconButton(
+                  icon: Icon(
+                    Icons.person,
+                    color: Colors.black,
+                  ),
+                  onPressed: () async {
+                    await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => SocialScreen()));
+                  },
+                  focusColor: Colors.lightGreen,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            right: 60,
+            bottom: 5,
+            child: SafeArea(
+              // avoids rounded screen problem
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.lightGreen,
+                  //shape: BoxShape.circle,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: IconButton(
+                  icon: Icon(
+                    Icons.settings,
+                    color: Colors.black,
+                  ),
+                  onPressed: () async {
+                    await Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Settings()));
+                  },
+                  focusColor: Colors.lightGreen,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+/*
+      floatingActionButton: FloatingActionButton.extended(
+        label: Text('Menu'),
         onPressed: () async {
           var navigationResult = await Navigator.push(
               context, new MaterialPageRoute(builder: (context) => Page2()));
@@ -43,25 +133,4 @@ class MapScreen extends StatelessWidget {
           }
         },
       ),
-      body: Container(
-        child: Center(
-          child: FlutterMap(
-            options: MapOptions(
-              center: LatLng(57.04, 9.92),
-              zoom: 12.0,
-            ),
-            layers: [
-              TileLayerOptions(
-                urlTemplate:
-                'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                subdomains: ['a', 'b', 'c'],
-                tileProvider: NetworkTileProvider(),
-              ),
-              //MarkerLayerOptions(markers: markers)
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+ */
