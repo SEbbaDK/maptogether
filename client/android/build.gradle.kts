d56723f26879338c26f1c28632aab287c8cd5344
SEbbaDK <sebbadk@gmail.com>
2021-03-30 10:14:30 +0200

    Set up flutter ui dependency in android client

--- client/android/build.gradle.kts
+++ client/android/build.gradle.kts
@@ -3,11 +3,21 @@ plugins {
     kotlin("android")
 }
 
+val storageUrl = System.getenv("FLUTTER_STORAGE_BASE_URL") ?: "https://storage.googleapis.com"
+repositories {
+	maven(url = "$projectDir/../ui/build/host/outputs/repo")
+	maven(url = "$storageUrl/download.flutter.io")
+}
+
 dependencies {
     implementation(project(":shared"))
     implementation("com.google.android.material:material:1.2.1")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.2")
+
+	debugImplementation("maptogether.client.ui:flutter_debug:1.0")
+	// profileImplementation("maptogether.client.ui:flutter_profile:1.0")
+	releaseImplementation("maptogether.client.ui:flutter_release:1.0")
 }
 
 android {
@@ -23,5 +33,13 @@ android {
         getByName("release") {
             isMinifyEnabled = false
         }
+		//profile {
+		// getByName("profile") {
+        //   initWith("debug")
+        // }
     }
+	compileOptions {
+		sourceCompatibility = JavaVersion.VERSION_1_8
+		targetCompatibility = JavaVersion.VERSION_1_8
+	}
 }
--- client/default.nix
+++ client/default.nix
@@ -1,8 +1,8 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/45a014f4796a0b3051652ae23d7bcb72823cb742.tar.gz") { config.android_sdk.accept_license = true; } }:
 let
-  btv = "29.0.2";
+  btv = "30.0.3";
   android = pkgs.androidenv.composeAndroidPackages {
-    buildToolsVersions = [ "28.0.3" btv ];
+    buildToolsVersions = [ "28.0.3" "29.0.2" btv ];
     platformVersions = [ "28" "29" ];
   };
   sdk = "${android.androidsdk}";
