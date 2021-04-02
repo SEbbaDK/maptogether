7a53adb9a79fd034a08f65dea17dd2acc2231791
Simon Svendsgaard Nielsen <simon97nielsen@gmail.com>
2021-04-02 17:52:31 +0200

    Added initial map pop-up

--- client/lib/widgets/map_screen_button_widgets/map.dart
+++ client/lib/widgets/map_screen_button_widgets/map.dart
@@ -1,3 +1,4 @@
+import 'package:client/widgets/map_screen_button_widgets/map_screen_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
@@ -12,13 +13,14 @@ class InteractiveMap extends StatefulWidget {
 }
 
 class _InteractiveMapState extends State<InteractiveMap> {
+  List<LatLng> taskPoints = [];
 
-  List<LatLng> tappedPoints = [];
+  LatLng popUpPositionOnMap = LatLng(0, 0);
+  bool showPopUp = false;
 
   @override
   Widget build(BuildContext context) {
-
-    var markers = tappedPoints.map((latlng) {
+    var markers = taskPoints.map((latlng) {
       return Marker(
         width: 40.0,
         height: 40.0,
@@ -29,37 +31,109 @@ class _InteractiveMapState extends State<InteractiveMap> {
       );
     }).toList();
 
-    void _handleTap(LatLng latlng) {
+    Widget selector = Container(
+      child: FittedBox(
+        child: Container(
+          decoration: BoxDecoration(
+            //color: Colors.grey,
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: MapScreenButton(
+                  child: Text('Tilføj POI'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: MapScreenButton(
+                  child: Text('Luk'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+
+    Marker popUpMarker = Marker(
+        height: 250,
+        width: 1000,
+        point: popUpPositionOnMap,
+        builder: (context) => Visibility(
+              visible: showPopUp,
+              child: FittedBox(
+                child: Stack(
+                  children: [
+                    Container(
+                      child: Center(
+                        child: Image(
+                          image: AssetImage('assets/MapTogether_popUp.png'),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 105,
+                      right: -40,
+                      child: SizedBox(height: 50, width: 200, child: selector),
+                    ),
+                  ],
+                ),
+              ),
+            ));
+
+    void _handleLongPress(LatLng latLng) {
       setState(() {
-        tappedPoints.add(latlng);
+        popUpPositionOnMap = latLng;
+        showPopUp = true;
+        //taskPoints.add(latLng);
       });
     }
 
-    return FlutterMap(
-      options: MapOptions(
+    return Stack(
+      children: [
+        FlutterMap(
+          options: MapOptions(
+            onLongPress: (latLng) {
+              _handleLongPress(latLng);
+            },
+            onTap: (latLng) {
+              setState(() {
+                showPopUp = false;
+              });
+            },
+            center: LatLng(57.04, 9.92),
+            // Aalborg
+            zoom: 12.0,
+            maxZoom: 22.0,
+          ),
+          layers: [
+            TileLayerOptions(
+              tileSize: 256,
+              urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+              subdomains: ['a', 'b', 'c'],
+              tileProvider: NetworkTileProvider(),
+              maxNativeZoom: 18,
+            ),
 
-        onLongPress: (latLng) {
-          print(latLng);
-          _handleTap(latLng);
-        },
-        center: LatLng(57.04, 9.92), // Aalborg
-        zoom: 12.0,
-        maxZoom: 22.0,
-      ),
-      layers: [
-        TileLayerOptions(
-          tileSize: 256,
-          urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-          subdomains: ['a', 'b', 'c'],
-          tileProvider: NetworkTileProvider(),
-          maxNativeZoom: 18,
+            MarkerLayerOptions(
+              markers: markers + [popUpMarker],
+            ),
+
+            //MarkerLayerOptions(markers: markers)
+          ],
         ),
 
-        MarkerLayerOptions(
-          markers: markers,
+        /*
+        Positioned(
+          child: SafeArea(child: selector),
+          left: 5,
+          bottom: 5,
         ),
 
-        //MarkerLayerOptions(markers: markers)
+         */
       ],
     );
   }
