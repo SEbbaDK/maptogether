0da37c56aec1217f889d1158c1ea20a8db51a7ba
SEbbaDK <sebbadk@gmail.com>
2021-04-23 16:15:34 +0200

    Implement changeset creation and set up binary file

--- osm_api/bin/osm_api.dart
+++ osm_api/bin/osm_api.dart
@@ -1,9 +1,64 @@
-import "package:osm_api/osm_api.dart";
+import 'dart:convert';
+import 'dart:io';
 
-void main() {
+import 'package:osm_api/osm_api.dart';
+import 'package:args/args.dart';
+
+var usage = '''
+USAGE:
+  osm_api COMMAND [PARAMS]
+
+COMMANDS:
+  help
+    Show this menu
+
+  map-by-box <left> <bottom> <right> <top>
+    Get the data in the bounding box (params are latlong coords)
+
+  create-changeset <comment> <createdBy>
+    Create a changeset and return api
+    comment:   The comment for the changeset
+    createdBy: The program creating this changeset
+''';
+
+void error(String message) {
+  stderr.write(message + '\n');
+  exit(1);
+}
+
+Future<dynamic> command(String command, List<String> args) {
   var api = OsmApi();
-  api.mapByBox(1.001, 1.001, 1.01, 1.01).then((map) {
-    print(map.elements.length);
-    print(map.elements.where((e) => e.isNode).map((v) => v.toJson()));
-  });
+
+  switch (command) {
+    case "help":
+      print(usage);
+      exit(0);
+      break;
+
+    case "map-by-box":
+      {
+        if (args.length != 4) error('map-by-box takes 4 parameters');
+        var vals = args.map((a) => double.parse(a)).toList();
+        return Function.apply(api.mapByBox, vals);
+      }
+
+    case "create-changeset":
+      {
+        if (args.length != 2) error('create-changeset takes 2 parameters');
+        return api.createChangeset(comment: args[0], createdBy: args[1]);
+      }
+
+      default: error('Unsupported command: $command'); break;
+  }
+
+  throw 'cannot happen';
+}
+
+int main(List<String> args) {
+  if (args.isEmpty) error('No argument given\n' + usage);
+
+  var c = args[0];
+  var a = (args.length == 1) ? <String>[] : args.skip(1).toList();
+  command(c, a).then((r) => print(jsonEncode(r.toJson())));
+  return 0;
 }
--- osm_api/lib/src/api.dart
+++ osm_api/lib/src/api.dart
@@ -5,16 +5,35 @@ import 'data.dart' as data;
 
 class OsmApi {
   static final String _url =
-      "https://master.apis.dev.openstreetmap.org/api/0.6/";
+      'https://master.apis.dev.openstreetmap.org/api/0.6/';
 
   Future<http.Response> _get(String path) =>
-      http.get(Uri.parse(_url + path), headers: {"Accept": "application/json"});
+      http.get(Uri.parse(_url + path), headers: {'Accept': 'application/json'});
+
+  Future<http.Response> _put(String path, Object data) =>
+      http.put(Uri.parse(_url + path), body: data);
 
   Future<data.MapData> mapByBox(
       double left, double bottom, double right, double top) async {
-    var response = await _get("map?bbox=${left},${bottom},${right},${top}");
+    var response = await _get('map?bbox=$left,$bottom,$right,$top');
     if (response.statusCode != 200) throw response.body;
     var json = jsonDecode(response.body);
     return data.MapData.fromJson(json);
+  }
+
+  Future<int> createChangeset({required String comment, required String createdBy}) async {
+    var response = await _put('changeset/create', '''
+		<osm>
+			<changeset>
+				<tag k="created_by" v="$createdBy"/>
+				<tag k="comment" v="$comment"/>
+			</changeset>
+		</osm>
+	    ''');
+    var id = int.tryParse(response.body);
+    if (response.statusCode != 200 || id == null)
+      throw 'Bad response: $response.body';
+    else
+      return id;
   }
 }
