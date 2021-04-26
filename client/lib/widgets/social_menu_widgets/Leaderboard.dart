cfb0012b422653fdbc8ed2cc2422015db20ada93
Hartvigen <hartvigen@gmail.com>
2021-04-26 14:49:03 +0200

    Leaderboard changed to StatefulWidget

--- client/lib/widgets/social_menu_widgets/Leaderboard.dart
+++ client/lib/widgets/social_menu_widgets/Leaderboard.dart
@@ -1,6 +1,7 @@
 import 'package:client/widgets/app_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 import 'User.dart';
 
@@ -13,38 +14,43 @@ class LeaderBoard{
   }
 }
 
-
-class LeaderBoardView extends StatelessWidget{
+class LeaderBoardView extends StatefulWidget{
   LeaderBoard leaderBoard;
 
   LeaderBoardView({Key key, @required this.leaderBoard}) : super(key: key);
 
+  @override
+  _LeaderBoardState createState() => _LeaderBoardState();
+
+}
+
+class _LeaderBoardState extends State<LeaderBoardView>{
 
   @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: MapTogetherAppBar(
-        title: "Leaderboard for " + leaderBoard.name,
+        title: "Leaderboard for " + widget.leaderBoard.name,
         actionButtons: [],
       ),
       body: Center(
         child: Container(
         margin: EdgeInsets.all(40.0),
         child: ListView.builder(
-            itemCount: leaderBoard.users.length,
+            itemCount: widget.leaderBoard.users.length,
             itemBuilder: (context, index){
               return Card(
                 child: ListTile(
                   title: Text("#"
                       + (index+1).toString()
                       + " "
-                      + leaderBoard.users[index].name
+                      + widget.leaderBoard.users[index].name
                       + " : "
-                      + leaderBoard.users[index].total.toString()
+                      + widget.leaderBoard.users[index].total.toString()
                       + " points"),
                   leading: CircleAvatar(
                     backgroundImage:
-                      AssetImage('assets/${leaderBoard.users[index].pfp}'),
+                      AssetImage('assets/${widget.leaderBoard.users[index].pfp}'),
                   ),
                 ),
               );
@@ -55,28 +61,3 @@ class LeaderBoardView extends StatelessWidget{
   }
 }
 
-/*class Participants extends StatefulWidget{
-  @override
-  _ParticipantsState createState() => _ParticipantsState();
-}
-
-class _ParticipantsState extends State<Participants>{
-  @override
-  Widget build(BuildContext context){
-    return Card(
-      child: ListTile(
-        title: Text("#"
-            + (index+1).toString()
-            + " "
-            + leaderBoard.users[index].name
-            + " : "
-            + leaderBoard.users[index].total.toString()
-            + " points"),
-        leading: CircleAvatar(
-          backgroundImage:
-          AssetImage('assets/${leaderBoard.users[index].pfp}'),
-        ),
-      ),
-    );
-  }
-}*/
