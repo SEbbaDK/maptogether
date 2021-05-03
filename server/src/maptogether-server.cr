371ded09e133d20f78b432be3f709eaab16e0d29
Thomas M.G <tgrose17@student.aau.dk>
2021-05-03 10:43:35 +0200

    update endpoints

--- server/src/maptogether-server.cr
+++ server/src/maptogether-server.cr
@@ -2,108 +2,75 @@ require "kemal"
 require "db"
 require "pg"
 require "json"
-require "../dbmock/*"
+require "./user.cr"
 
 
 module MapTogether::Server
 
-    DB.connect "postgres://maptogether:maptogether@localhost:5432/maptogether?retry_attempts=100" do |cnn|
-      cnn.exec "CREATE SEQUENCE IF NOT EXISTS user_id start 1"
-      puts typeof(cnn)
-      cnn.exec "CREATE TABLE IF NOT EXISTS users (
-      userID integer NOT NULL DEFAULT nextval('user_id') PRIMARY KEY,
-      name varchar(20),
-      score integer)"
-      puts typeof(cnn)
-      cnn.exec "insert into users values (DEFAULT, $1, $2)", "Hanne", 69420
-      DbMock.mockUsers(cnn)
-      cnn.query "select userID, name, score from users order by userID asc" do |ra|
-        ra.each do
-          id = ra.read(Int32)
-          name = ra.read(String)
-          score = ra.read(Int32)
-          puts "(#{id}) #{name} (#{score})"
-        end
-      end
-      cnn.exec "DELETE FROM users where userID > 3"
-      cnn.query "select userID, name, score from users order by userID asc" do |ra2|
-          ra2.each do
-              id = ra2.read(Int32)
-              name = ra2.read(String)
-              score = ra2.read(Int32)
-              puts "(#{id}) #{name} (#{score})"
-          end
-      end
+	module Endpoints
+		address = "postgres://maptogether:maptogether@localhost:5432/maptogether?retry_attempts=100"
 
-      #loop do
-         # pp cnn.scalar("SELECT NOW()")
-          #sleep 0.5
-        #end
+		# Request data about a specific user (id, name, score, achievements and followers)
+		get "/user/:id" do |env|
+			user = User.new
+			id = env.params.url["id"]
+			DB.connect address do |db|
+				user.userID, user.name = db.query_one "SELECT userID, name FROM users WHERE userID = $1 limit 1", id, as: {Int32, String}
+				user.score = db.query_one "SELECT SUM (score) AS score FROM contributions WHERE userID = $1", id, as: {Int32}
+				
+				user.achievements = [] of String
+				db.query "SELECT achievement FROM unlocked WHERE userID = $1", id do |rows|
+					rows.each do
+						user.achievements << rows.read(String)
+					end
+				end
 
-    end
+				user.followers = [] of User
+				db.query "SELECT userID, name FROM follows INNER JOIN users ON follower = userID WHERE followee = $1", id do |rows|
+					rows.each do
+						user.followers << User.new(userID: rows.read(Int32), name: rows.read(String))
+					end
+				end
 
-    get "/" do
-        "hi"
-    end
+				user.following = [] of User
+				db.query "SELECT userID, name FROM follows INNER JOIN users ON followee = userID WHERE follower = $1", id do |rows|
+					rows.each do
+						user.following << User.new(userID: rows.read(Int32), name: rows.read(String))
+					end
+				end
+			end
 
-    get "/user/:id" do |env|
-        string = JSON.build do |json|
-            json.object do
-                id = env.params.url["id"]
-                DB.connect "postgres://maptogether:maptogether@localhost:5432/maptogether?retry_attempts=100" do |cnn|
-                    cnn.query "select userID, name, score from users where userID = $1", id do |rs|
-                        rs.each do
-                            userid = rs.read(Int32)
-                            name = rs.read(String)
-                            score = rs.read(Int32)
-                            json.field "id", userid
-                            json.field "name", name
-                            json.field "score", score
-                            puts "#{userid} #{name} #{score}"
-                        end
-                    end
-                end
-            end
-        end
-        string
-    end
+			JSON.build do |json|
+				user.toJson(json)
+			end
+		end
 
-    get "/leaderboard/global/all_time" do |env|
-        string = JSON.build do |json|
-            counter = 1
-            json.object do
-                DB.connect "postgres://maptogether:maptogether@localhost:5432/maptogether?retry_attempts=100" do |cnn|
-                    cnn.query "SELECT userID, name, score FROM users ORDER BY score DESC" do |users|
-                        json.field "users" do
-                            json.array do
-                                users.each do
-                                    userid = users.read(Int32)
-                                    name = users.read(String)
-                                    score = users.read(Int32)
-                                    json.object do
-                                        json.field "placement", counter
-                                        json.field "userID", userid
-                                        json.field "name", name
-                                        json.field "score", score
-                                    end
-                                    counter += 1
-                                end
-                            end
-                        end
-                    end
-                end
-            end
-        end
-        string
-    end
-
-    get "/" do |env|
-
-    end
-
-    Kemal.run 8080
+		# Retrieve all users' id, name and score
+		get "/leaderboard/global/all_time" do |env|
+			JSON.build do |json|
+				json.object do
+					DB.connect address do |db|
+						db.query "SELECT userID, name, score FROM (SELECT userID, SUM (score) AS score FROM contributions GROUP BY userID) AS s INNER JOIN users AS u ON u.userID = s.userID ORDER BY score DESC" do |rows|
+							json.field "users" do
+								rows.each do
+									json.array do
+										User.new(userID: rows.read(Int32), name: rows.read(String), score: rows.read(Int32)).toJson(json)
+									end
+								end
+							end
+						end
+					end
+				end
+			end
+		end
+		
+		# Test endpoint
+		get "/" do
+			"hi"
+		end
+	end
 
 
-
+	Kemal.run 8080
 end
 
