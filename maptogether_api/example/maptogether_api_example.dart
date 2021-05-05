1da9af46e3eada52be53d61213cecb2a192b021b
SEbbaDK <sebbadk@gmail.com>
2021-05-05 15:05:55 +0200

    maptogether_api: Finish basic get string access

--- maptogether_api/example/maptogether_api_example.dart
+++ maptogether_api/example/maptogether_api_example.dart
@@ -1,6 +1,7 @@
 import 'package:maptogether_api/maptogether_api.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+	final api = MapTogetherApi();
+	api.user(1).then(print);
+	api.globalLeaderboard(LeaderboardType.all_time).then(print);
 }
--- maptogether_api/lib/src/api.dart
+++ maptogether_api/lib/src/api.dart
@@ -1,5 +1,51 @@
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+
+enum LeaderboardType { weekly, montly, all_time }
+String stringify(LeaderboardType type) {
+    switch(type) {
+        case LeaderboardType.weekly: return "weekly";
+        case LeaderboardType.montly: return "montly";
+        case LeaderboardType.all_time: return "all_time";
+    }
+    throw 'Missing case';
+}
+
+class InvalidRegionException implements Exception {
+    final String _message;
+	InvalidRegionException([this._message = 'Region cannot be global or leaderboard']);
+	@override String toString() => _message;
+}
 
 class MapTogetherApi {
-  bool get isAwesome => true;
+  final String _url = 'https://maptogether.sebba.dk/api/';
+
+  Future<http.Response> _get(String path) =>
+  	http.get(Uri.parse(_url + path));
+
+  String Function(http.Response) _checkRequest(String description) =>
+  	(http.Response res) {
+    	if (res.statusCode != 200)
+        	throw Exception('HTTP error ${res.statusCode} when $description');
+        else
+        	return res.body;
+  	};
+
+  Future<String> user(int id) =>
+  	_get('user/$id').then(_checkRequest('getting user'));
+
+  Future<String> leaderboard(String base, LeaderboardType type) =>
+  	_get('leaderboard/$base/${stringify(type)}').then(_checkRequest('getting leaderboard'));
+
+  Future<String> personalLeaderboard(LeaderboardType type) =>
+  	leaderboard('personal', type);
+
+  Future<String> globalLeaderboard(LeaderboardType type) =>
+  	leaderboard('global', type);
+
+  Future<String> regionalLeaderboard(String region, LeaderboardType type) {
+    if (region == 'personal' || region == 'global')
+        throw InvalidRegionException();
+    else
+    	return leaderboard(region, type);
+  }
 }
