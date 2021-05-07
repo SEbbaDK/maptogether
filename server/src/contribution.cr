64cb214ea9480e6f9638e71767a60c56d8820f5b
Thomas M.G <tgrose17@student.aau.dk>
2021-05-07 10:11:52 +0200

    require contribution fields except for id and use parameter names

--- server/src/contribution.cr
+++ server/src/contribution.cr
@@ -1,29 +1,29 @@
 class Contribution
     property contribution_id : Nil | Int64
-    property user_id : Nil | Int64
-    property type : Nil | Int64
-    property changeset : Nil | Int64
-    property score : Nil | Int64
-    property date_time : Nil | Time
+    property user_id : Int64
+    property type : Int64
+    property changeset : Int64
+    property score : Int64
+    property date_time : Time
 
     def initialize (
-            #@contribution_id = nil,
-            @user_id = nil,
-            @type = nil,
-            @changeset = nil,
-            @score = nil,
-            @date_time = nil
+            @user_id,
+            @type,
+            @changeset,
+            @score,
+            @date_time,
+            @contribution_id = nil,
         )
     end
 
     def self.from_json (json_obj)
-        Contribution.new(
-            #contribution_id = json_obj["contributionid"].as(Int64),
-            user_id = json_obj["id"].as(Int64),
-            type = json_obj["type"].as(Int64),
-            changeset = json_obj["changeset"].as(Int64),
-            score = json_obj["score"].as(Int64),
-            date_time = Time.parse_iso8601(json_obj["datetime"].as(String))
+        #contribution_id: json_obj["contributionid"]?.as(Int64 | Nil),
+        contribution = Contribution.new(
+            user_id:json_obj["id"].as(Int64),
+            type: json_obj["type"].as(Int64),
+            changeset:json_obj["changeset"].as(Int64),
+            score: json_obj["score"].as(Int64),
+            date_time: Time.parse_iso8601(json_obj.["datetime"].as(String)),
         )
     end
 end
