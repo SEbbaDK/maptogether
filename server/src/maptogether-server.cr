81c36494a492d19ffd2ad2d3851bf4d93f984e30
Thomas M.G <tgrose17@student.aau.dk>
2021-05-14 12:25:59 +0200

    add put user, follows and delete follows

--- server/src/maptogether-server.cr
+++ server/src/maptogether-server.cr
@@ -36,35 +36,67 @@ module MapTogether::Server
 				raise DB::ConnectionRefused.new("Could not connect to database")
 			end
 		end
-
+		
 		# Request data about a specific user (id, name, score, achievements and followers)
 		get "/user/:id" do |env|
 			user = User.new
 			id = env.params.url["id"]
-
+			
 			try_open_connection do |db|
 				user.user_id, user.name = db.query_one Queries::USER_FROM_ID, id, as: {Int64, String}
 				user.score = db.query_one Queries::TOTAL_SCORE_FROM_ID, id, as: {Int64}
-
+				
 				user.achievements = [] of Achievement
 				db.query Queries::ACHIEVEMENTS_FROM_ID, id do |rows|
 					rows.each do
 						user.achievements << Achievement.new(rows.read(String), rows.read(String))
 					end
 				end
-
+				
 				user.followers = [] of User
 				db.query Queries::FOLLOWERS_FROM_ID, id do |rows|
 					rows.each do
 						user.followers << User.new(user_id: rows.read(Int64), name: rows.read(String))
 					end
 				end
-
+				
 				user.following = [] of User
 				db.query Queries::FOLLOWING_FROM_ID, id do |rows|
 					rows.each do
 						user.following << User.new(user_id: rows.read(Int64), name: rows.read(String))
 					end
+				end
+			end
+
+			put "/user/:id/following/:followee" do |env|
+				id = env.params.url["id"]
+				followee = env.params.url["followee"]
+				
+				# TODO: Does this throw a nice error if the user does not exist? else make some check
+				try_open_connection do |db|
+					#db.query_one Queries::USER_FROM_ID, id, as: {Int64, String}
+					db.exec "INSERT INTO follows (follower, followee) VALUES ($1, $2)", id, followee
+				end
+			end
+
+			delete "user/:id/following/:followee" do |env|
+				id = env.params.url["id"]
+				followee = env.params.url["followee"]
+
+				try_open_connection do |db|
+					# TODO: Does this throw a nice error if the follows does not exist? else make some check
+					#db.query_one "SELECT follower, followee FROM follows WHERE follower = $1 AND followee = $2", id, followee as: {Int64, Int64}
+					result = db.exec "DELETE FROM follows WHERE follower = $1 AND followee = $2", id, followee
+					raise "Oops deleted ${result.rows_affected} rows" if result.rows_affected != 1
+				end
+			end
+
+			put "/user/:id/:name" do |env|
+				id = env.params.url["id"]
+				name = env.params.url["name"]
+
+				try_open_connection do |db|
+					db.exec "INSERT INTO users (userID, name) VALUES ($1, $2)", id, name
 				end
 			end
 
@@ -126,6 +158,7 @@ module MapTogether::Server
 	            	contribution.date_time
 	        end
 	    end
+
 
 		# Test endpoint
 		get "/" do |env|
