1c017982bd76163069baf75f291c312a7cd92ecf
SEbbaDK <sebbadk@gmail.com>
2021-05-18 12:28:27 +0200

    mtapi: Format all code

--- maptogether_api/lib/src/api.dart
+++ maptogether_api/lib/src/api.dart
@@ -30,22 +30,20 @@ class Api {
   Api(String access) : _access = access;
 
   Map<String, String> _authHeader(String? auth) =>
-  	{ 'Authorization': 'Basic ${auth != null ? auth : _access}' };
+      {'Authorization': 'Basic ${auth != null ? auth : _access}'};
 
   // Internal http methods
-  
-  Future<http.Response> _get(String path) =>
-  	http.get(Uri.parse(_url + path));
 
-  Future<http.Response> _put(String path, { String? auth }) =>
-	http.put(Uri.parse(_url + path), headers: _authHeader(auth));
+  Future<http.Response> _get(String path) => http.get(Uri.parse(_url + path));
 
-  Future<http.Response> _del(String path, { String? auth }) =>
-  	http.delete(Uri.parse(_url + path), headers: _authHeader(auth));
+  Future<http.Response> _put(String path, {String? auth}) =>
+      http.put(Uri.parse(_url + path), headers: _authHeader(auth));
 
+  Future<http.Response> _del(String path, {String? auth}) =>
+      http.delete(Uri.parse(_url + path), headers: _authHeader(auth));
 
   // Stream handlers
-  
+
   String Function(http.Response) _checkRequest(String description) =>
       (http.Response res) {
         if (res.statusCode != 200)
@@ -54,12 +52,10 @@ class Api {
           return res.body;
       };
 
-  data.User _decodeUser(String input) =>
-      data.User.fromJson(jsonDecode(input));
+  data.User _decodeUser(String input) => data.User.fromJson(jsonDecode(input));
 
   data.Leaderboard _decodeLeaderboard(String input) =>
       data.Leaderboard.fromJson(jsonDecode(input));
-
 
   // Pull Endpoints
 
@@ -68,7 +64,8 @@ class Api {
 
   Future<data.Leaderboard> leaderboard(String base, LeaderboardType type) =>
       _get('leaderboard/$base/${stringify(type)}')
-          .then(_checkRequest('getting leaderboard')).then(_decodeLeaderboard);
+          .then(_checkRequest('getting leaderboard'))
+          .then(_decodeLeaderboard);
 
   Future<data.Leaderboard> personalLeaderboard(LeaderboardType type) =>
       leaderboard('personal', type);
@@ -76,23 +73,25 @@ class Api {
   Future<data.Leaderboard> globalLeaderboard(LeaderboardType type) =>
       leaderboard('global', type);
 
-  Future<data.Leaderboard> regionalLeaderboard(String region, LeaderboardType type) {
+  Future<data.Leaderboard> regionalLeaderboard(
+      String region, LeaderboardType type) {
     if (region == 'personal' || region == 'global')
       throw InvalidRegionException();
     else
       return leaderboard(region, type);
   }
 
-
   // Push Endpoints
 
-  Future<void> createUser(int id, String secret, String clientToken, String clientSecret) =>
-  	_put('user/$id', auth: 'Basic $_access $secret $clientToken $clientSecret')
-  		.then(_checkRequest('creating user'));
+  Future<void> createUser(
+          int id, String secret, String clientToken, String clientSecret) =>
+      _put('user/$id',
+              auth: 'Basic $_access $secret $clientToken $clientSecret')
+          .then(_checkRequest('creating user'));
 
   Future<void> follow(int id, int other) =>
-  	_put('user/$id/following/$other').then(_checkRequest('following a user'));
+      _put('user/$id/following/$other').then(_checkRequest('following a user'));
 
-  Future<void> unfollow(int id, int other) =>
-    _del('user/$id/following/$other').then(_checkRequest('unfollowing a user'));
+  Future<void> unfollow(int id, int other) => _del('user/$id/following/$other')
+      .then(_checkRequest('unfollowing a user'));
 }
--- maptogether_api/test/api_test.dart
+++ maptogether_api/test/api_test.dart
@@ -1,14 +1,13 @@
 import 'package:test/test.dart';
 import 'package:maptogether_api/src/api.dart' as mt;
 
-
-void main(){
+void main() {
   group('Maptogether reads', () {
     final api = mt.Api('non-functional-key');
 
     test('Fetching user score', () async {
       var result = (await api.user(1));
-      expect(result , isNotNull);
+      expect(result, isNotNull);
     });
 
     test('Fetching Leaderboard', () async {
@@ -16,8 +15,9 @@ void main(){
       expect(result, isNotNull);
     });
 
-    test('Erroring when updating user', () async =>
-      expect(() async => await api.createUser(1, "", "", ""), throwsException)
-    );
+    test(
+        'Erroring when updating user',
+        () async => expect(
+            () async => await api.createUser(1, "", "", ""), throwsException));
   });
 }
