ef5846edb97142b5d2c06a9542e34bac8ab31f65
SEbbaDK <sebbadk@gmail.com>
2021-05-19 15:23:20 +0200

    client: Refactored and formatted leaderboard screen

--- client/lib/widgets/social_menu_widgets/Leaderboard.dart
+++ client/lib/widgets/social_menu_widgets/Leaderboard.dart
@@ -1,77 +1,58 @@
-import 'package:client/widgets/app_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:client/database.dart';
 import 'package:maptogether_api/maptogether_api.dart';
 import 'package:provider/provider.dart';
 
-import '../../data_fetchers.dart';
-import 'User.dart';
+import 'package:client/database.dart';
+import 'package:client/widgets/app_bar.dart';
+import 'package:client/data_fetchers.dart';
+import 'package:client/widgets/social_menu_widgets/User.dart';
 
-
-class LeaderBoardView extends StatelessWidget{
+class LeaderBoardView extends StatelessWidget {
   int leaderboardIndex;
   String leaderBoardName;
   LeaderboardType type;
-  LeaderBoardView({Key key, @required this.leaderBoardName, @required this.type}) : super(key: key);
+  LeaderBoardView(
+      {Key key, @required this.leaderBoardName, @required this.type})
+      : super(key: key);
+
+  Widget scoreWidget(int placement, String name, int score) => Card(
+        child: ListTile(
+          title: Text("#$placement $name : $score"),
+          leading: CircleAvatar(
+            backgroundImage: AssetImage('assets/business.png'),
+          ),
+        ),
+      );
+
+  Widget leaderboard(Leaderboard leaderboard) => Scaffold(
+      appBar: MapTogetherAppBar(
+        title: "Leaderboard for " + leaderBoardName,
+        actions: [],
+      ),
+      body: Column(children: <Widget>[
+        Expanded(
+          child: ListView.builder(
+              itemCount: leaderboard.entries.length,
+              itemBuilder: (context, index) => scoreWidget(
+                  index + 1,
+                  leaderboard.entries[index].user.name,
+                  leaderboard.entries[index].score)),
+        ),
+      ]));
 
   @override
-  Widget build(BuildContext context){
-
+  Widget build(BuildContext context) {
     return FutureBuilder(
         future: getLeaderboard(type, leaderBoardName),
         builder: (BuildContext context, AsyncSnapshot<Leaderboard> snapshot) {
-          if(snapshot.hasData) {
-            return Scaffold(
-                appBar: MapTogetherAppBar(
-                  title: "Leaderboard for " + leaderBoardName,
-                  actions: [],
-                ),
-                body: Center(
-                    child: Container(
-                        margin: EdgeInsets.all(40.0),
-                        child: Column(
-                            children: <Widget>[
-                              Expanded(
-                                child: ListView.builder(
-                                    itemCount: snapshot.data.entries.length,
-                                    itemBuilder: (context, index) {
-                                      return Card(
-                                        child: ListTile(
-                                          title: Text("#"
-                                              + (index + 1).toString()
-                                              + " "
-                                              + snapshot.data.entries[index].user.name
-                                              + " : "
-                                              + snapshot.data.entries[index].score.toString()
-                                              + " points"),
-
-                                          leading: CircleAvatar(
-                                            backgroundImage:
-                                            AssetImage('assets/business.png'),
-
-                                          ),
-                                        ),
-                                      );
-                                    }),
-                              ),
-                            ]
-                        )
-                    )
-                )
-            );
-          }
-          else if(snapshot.hasError)
+          if (snapshot.hasData)
+            return leaderboard(snapshot.data);
+          else if (snapshot.hasError)
             return errorData();
-
-          else {
-            return Expanded(
-                flex: 14,
-                child: waitingLoop());
-          }
-        }
-        );
-
+          else
+            return Expanded(flex: 14, child: waitingLoop());
+        });
   }
 }
