239130d031a8fd5ecb5b5a91796ef52fdb472d0d
Hartvigen <hartvigen@gmail.com>
2021-05-19 10:32:15 +0200

    follow list is loaded as single entity

--- client/lib/widgets/social_menu_widgets/friends.dart
+++ client/lib/widgets/social_menu_widgets/friends.dart
@@ -15,96 +15,98 @@ class Friends extends StatelessWidget {
     final api = MapTogetherApi();
 
     return Container(
-      child: Column(
-        children: <Widget>[
-          FutureBuilder<User>(
+        child: FutureBuilder<User>(
             future: getUser(),
             builder: (BuildContext context, AsyncSnapshot<User> user) {
               if (user.hasData) {
-                return Expanded(
-                    flex: 14,
-                    child: ListView.separated(
-                        separatorBuilder: (BuildContext context, int index) =>
-                        const Divider(thickness: 2, height: 2),
-                        itemCount: user.data.following.length,
-                        itemBuilder: (context, index) {
-                          return ListTile(
-                            onLongPress: () {
-                              showModalBottomSheet <void>(
-                                  context: context,
-                                  builder: (BuildContext context) {
-                                    return Container(
-                                      height: 100,
-                                      color: Colors.orange,
-                                      child: Center(
-                                        child: TextButton(
-                                          child: Text("Unfollow"),
-                                          style: TextButton.styleFrom(
-                                              primary: Colors.white,
-                                              backgroundColor: Colors.red
+                return Column(
+                  children: <Widget>[
+                    Expanded(
+                        flex: 14,
+                        child: ListView.separated(
+                            separatorBuilder: (BuildContext context,
+                                int index) =>
+                            const Divider(thickness: 2, height: 2),
+                            itemCount: user.data.following.length,
+                            itemBuilder: (context, index) {
+                              return ListTile(
+                                onLongPress: () {
+                                  showModalBottomSheet <void>(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return Container(
+                                          height: 100,
+                                          color: Colors.orange,
+                                          child: Center(
+                                            child: TextButton(
+                                              child: Text("Unfollow"),
+                                              style: TextButton.styleFrom(
+                                                  primary: Colors.white,
+                                                  backgroundColor: Colors.red
+                                              ),
+                                              onPressed: () {
+                                                context
+                                                    .read<DummyDatabase>()
+                                                    .followingNames
+                                                    .remove(context
+                                                    .read<DummyDatabase>()
+                                                    .following[index].name);
+                                                context
+                                                    .read<DummyDatabase>()
+                                                    .following
+                                                    .removeAt(index);
+                                                Navigator.pop(context);
+                                              },
+                                            ),
                                           ),
-                                          onPressed: () {
-                                            context
-                                                .read<DummyDatabase>()
-                                                .followingNames
-                                                .remove(context
-                                                .read<DummyDatabase>()
-                                                .following[index].name);
-                                            context
-                                                .read<DummyDatabase>()
-                                                .following
-                                                .removeAt(index);
-                                            Navigator.pop(context);
-                                          },
-                                        ),
-                                      ),
-                                    );
-                                  }
+                                        );
+                                      }
+                                  );
+                                },
+                                title: Text(user.data.following[index].name),
+                                leading: CircleAvatar(
+                                  backgroundImage:
+                                  AssetImage('assets/business.png'),
+                                ),
                               );
-                            },
-                            title: Text(user.data.following[index].name),
-                            leading: CircleAvatar(
-                              backgroundImage:
-                              AssetImage('assets/business.png'),
+                            }
+                        )
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Container(
+                          color: Colors.lightGreen,
+                          child: TextButton(
+                            child: Text(
+                              'Follow New',
+                              style: TextStyle(
+                                  fontSize: 20.0, color: Colors.white),
                             ),
-                          );
-                        }
-                    )
+                            onPressed: () {
+                              Navigator.push(context,
+                                  MaterialPageRoute(
+                                      builder: (context) => NewFriend()));
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 );
               }
               else {
                 return Expanded(
-                  flex: 14,
-                  child: SizedBox(
-                    child: CircularProgressIndicator(),
-                    width: 60,
-                    height: 60)
+                    flex: 14,
+                    child: SizedBox(
+                        child: CircularProgressIndicator(),
+                        width: 60,
+                        height: 60)
                 );
               }
             }
-            ),
-            Expanded(
-              flex: 2,
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Container(
-                  color: Colors.lightGreen,
-                  child: TextButton(
-                    child: Text(
-                      'Follow New',
-                      style: TextStyle(fontSize: 20.0, color: Colors.white),
-                    ),
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => NewFriend()));
-                    },
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      );
-    }
+        ));
   }
+}
 
