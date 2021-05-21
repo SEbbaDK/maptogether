bd258193b25d6bd04332796417f99a09cf561120
Thomas M.G <tgrose17@student.aau.dk>
2021-05-21 14:12:15 +0200

    add monthly and weekly score to mtapi test

--- maptogether_api/test/data_test.dart
+++ maptogether_api/test/data_test.dart
@@ -3,26 +3,35 @@ import 'package:maptogether_api/src/data.dart' as data;
 
 void main() {
   group('User', () {
-    var user = data.User(id: 1, score: 12, name: 'Jens', achievements: [
-      data.Achievement(
-        name: 'fdf',
-        description: 'Stuff',
-      )
-    ], followers: [
-      data.SimpleUser(
+    var user = data.User(
         id: 1,
-        name: 'Hej',
-      )
-    ], following: [
-      data.SimpleUser(id: 2, name: 'Med')
-    ], leaderboards: [
-      data.LeaderboardSummary(
-          path: "/leaderboard/all_time/global",
-          name: "Global",
-          rank: 1,
-          total: 20,
-          type: data.LeaderboardType.all_time)
-    ]);
+        scoreAllTime: 12,
+        scoreMonthly: 5,
+        scoreWeekly: 2,
+        name: 'Jens',
+        achievements: [
+          data.Achievement(
+            name: 'fdf',
+            description: 'Stuff',
+          )
+        ],
+        followers: [
+          data.SimpleUser(
+            id: 1,
+            name: 'Hej',
+          )
+        ],
+        following: [
+          data.SimpleUser(id: 2, name: 'Med')
+        ],
+        leaderboards: [
+          data.LeaderboardSummary(
+              path: "/leaderboard/all_time/global",
+              name: "Global",
+              rank: 1,
+              total: 20,
+              type: data.LeaderboardType.all_time)
+        ]);
 
     test('have', () => expect(user.followers.length, 1));
   });
