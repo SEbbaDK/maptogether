f2397790e803204b30b3b34bf623d598934f9bf3
Simon Svendsgaard Nielsen <simon97nielsen@gmail.com>
2021-05-22 21:15:11 +0200

    Reformat

--- client/lib/quests/quest_handler.dart
+++ client/lib/quests/quest_handler.dart
@@ -2,7 +2,6 @@ import 'package:client/quests/bench_quests/backrest_bench_quest.dart';
 import 'package:client/quests/building_quests/building_type_quest.dart';
 import 'package:client/quests/quest.dart';
 import 'package:flutter/material.dart';
-import 'package:latlong/latlong.dart';
 import 'package:osm_api/osm_api.dart' as osm;
 
 class QuestHandler extends ChangeNotifier {
@@ -11,21 +10,24 @@ class QuestHandler extends ChangeNotifier {
   Set<Quest> quests = Set();
 
   final finders = [
-  	BackrestBenchQuestFinder(),
-  	BuildingTypeQuestFinder(),
+    BackrestBenchQuestFinder(),
+    BuildingTypeQuestFinder(),
   ];
 
   // Finding quests within bound
-  Future<void> loadQuests(double left, double bottom, double right, double top) async {
-    // Fetch elements within bound
+  Future<void> loadQuests(
+      double left, double bottom, double right, double top) async {
     api = osm.Api(
         'id', osm.Auth.getUnauthorizedClient(), osm.ApiEnv.dev('master'));
 
+    // Fetch elements within bound
     List<osm.Element> elements =
         (await api.mapByBox(left, bottom, right, top)).elements;
 
-	elements.forEach((e) => finders.forEach((f) { if (f.applicable(e)) quests.add(f.construct(e)); }));
-	
+    elements.forEach((e) => finders.forEach((f) {
+          if (f.applicable(e)) quests.add(f.construct(e));
+        }));
+
     notifyListeners();
   }
 
