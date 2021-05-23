9d40ec0582808fc1ca810efc71935bd6d4576390
Simon Svendsgaard Nielsen <simon97nielsen@gmail.com>
2021-05-23 09:49:50 +0200

    operator == defined for Quests

--- client/lib/quests/quest.dart
+++ client/lib/quests/quest.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:latlong/latlong.dart';
 import 'package:osm_api/osm_api.dart' as osm;
+import 'package:quiver/core.dart';
 
 abstract class Quest {
   osm.Element element;
@@ -14,4 +14,18 @@ abstract class Quest {
   String question();
 
   Future<void> solve(osm.Api api, String possibility);
+
+  @override
+  bool operator ==(Object that) {
+    return that is Quest &&
+        this.element.type == that.element.type &&
+        this.element.id == that.element.id &&
+        this.question() == that.question();
+  }
+
+  // Inspired by this: https://stackoverflow.com/questions/20577606/whats-a-good-recipe-for-overriding-hashcode-in-dart
+  @override
+  int get hashCode {
+    return hash2(element.type.hashCode, element.type.hashCode);
+  }
 }
